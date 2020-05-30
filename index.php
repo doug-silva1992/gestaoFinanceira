6a0ec008c41be21796f2cf82f21093c937254699
dvalverde <dvalverde>
2020-05-30 19:19:37 -0300

    Criado a página inicial

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    include_once('head.html');
+?>
+
+<div class="conteiner">
+    <h2 align = "center">Escolha o Método de Avaliação de Investimento</h2>
+    <div class="list-group">
+    <a href="#" class="list-group-item list-group-item-action">Pay-Back (prazo de retorno)</a>
+    <a href="#" class="list-group-item list-group-item-action">Valor Presente Líquido (VPL)</a>
+    <a href="#" class="list-group-item list-group-item-action">Taxa Interna de Retorno</a>
+    </div>
+</div>
+
+<?php
+    include_once('footer.html');
+?>
